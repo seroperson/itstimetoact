50b90d2edaf3b030a7b582d39b97467bc17d829e
seroperson <seroperson@gmail.com>
2016-02-12 20:36:57 +0300

    Added some base storing api in TimeToAct

--- lib/src/main/java/com/seroperson/itstimetoact/TimeToAct.java
+++ lib/src/main/java/com/seroperson/itstimetoact/TimeToAct.java
@@ -1,10 +1,41 @@
 package com.seroperson.itstimetoact;
 
+import com.seroperson.itstimetoact.event.ActEvent;
+
 import android.content.Context;
 
-public class TimeToAct {
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class TimeToAct {
+
+    private final Context context;
+    private final Map<String, ActEvent> eventMap = new HashMap<String, ActEvent>();
 
     public TimeToAct(Context context) {
+        if(context == null) {
+            throw new IllegalArgumentException("");
+        }
+        this.context = context;
     }
+
+    public void loadStoredEventData() {
+        eventMap.clear();
+
+        Set<ActEvent> loadedSet = loadStoredEventData(context);
+        for(ActEvent event : loadedSet) {
+            eventMap.put(event.getEventKey(), event);
+        }
+    }
+
+    public void storeEventData() {
+        storeEventData(eventMap.values(), context);
+    }
+
+    protected abstract Set<ActEvent> loadStoredEventData(Context context);
+
+    protected abstract void storeEventData(Collection<ActEvent> eventSet, Context context);
 
 }
--- lib/src/main/java/com/seroperson/itstimetoact/event/ActEvent.java
+++ lib/src/main/java/com/seroperson/itstimetoact/event/ActEvent.java
@@ -7,6 +7,9 @@ public abstract class ActEvent {
     private final String eventKey;
 
     public ActEvent(String eventKey) {
+        if(eventKey == null) {
+            throw new IllegalArgumentException("");
+        }
         this.eventKey = eventKey;
     }
 
@@ -18,6 +21,23 @@ public abstract class ActEvent {
 
     public String getEventKey() {
         return eventKey;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) {
+            return true;
+        }
+        if(o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ActEvent actEvent = (ActEvent) o;
+        return eventKey.equals(actEvent.eventKey);
+    }
+
+    @Override
+    public int hashCode() {
+        return eventKey.hashCode();
     }
 
 }
