dfd702ac53a482de49774d7982a844b1b6be2588
seroperson <seroperson@gmail.com>
2016-03-20 05:11:05 +0300

    Gson serialization fix

--- itstimetoact-gson/src/main/java/com/seroperson/itstimetoact/gson/GsonTimeToAct.java
+++ itstimetoact-gson/src/main/java/com/seroperson/itstimetoact/gson/GsonTimeToAct.java
@@ -4,7 +4,7 @@ import android.content.Context;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.stream.JsonReader;
+import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonWriter;
 import com.seroperson.itstimetoact.InFileTimeToAct;
 import com.seroperson.itstimetoact.event.ActEvent;
@@ -14,13 +14,15 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 public class GsonTimeToAct extends InFileTimeToAct {
 
-    private final Gson gson = createGson(new GsonBuilder());
+    private final static Type defaultType = new TypeToken<Set<ActEvent>>() { }.getType();
+    private final Gson gson = createGsonBuilder(new GsonBuilder()).create();
 
     public GsonTimeToAct(Context context) {
         super(context);
@@ -28,22 +30,21 @@ public class GsonTimeToAct extends InFileTimeToAct {
 
     @Override
     protected Set<ActEvent> loadEventData(File storage) {
-        JsonReader reader = null;
+        FileReader reader = null;
         try {
             try {
-                reader = new JsonReader(new FileReader(storage));
-                reader.beginObject();
-
-                return gson.fromJson(reader, Class.forName(reader.nextName()));
+                reader = new FileReader(storage);
+                return gson.fromJson(reader, defaultType);
             } catch (FileNotFoundException e) {
                 // impossible
-            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
             } finally {
                 if (reader != null) {
                     reader.close();
                 }
             }
         } catch (IOException e) {
+            e.printStackTrace();
         }
         return new HashSet<ActEvent>();
     }
@@ -54,30 +55,25 @@ public class GsonTimeToAct extends InFileTimeToAct {
         try {
             try {
                 writer = new JsonWriter(new FileWriter(storage));
-
-                Class<?> type = eventSet.getClass();
-
-                writer.beginObject();
-                writer.name(type.getName());
-                gson.toJson(eventSet, type, writer);
-                writer.endObject();
+                gson.toJson(eventSet, defaultType, writer);
                 writer.flush();
-
                 return true;
             } catch (FileNotFoundException e) {
                 // impossible
+                e.printStackTrace();
             } finally {
                 if (writer != null) {
                     writer.close();
                 }
             }
         } catch (IOException e) {
+            e.printStackTrace();
         }
         return false;
     }
 
-    protected Gson createGson(GsonBuilder gsonBuilder) {
-        return gsonBuilder.create();
+    protected GsonBuilder createGsonBuilder(GsonBuilder gsonBuilder) {
+        return gsonBuilder;
     }
 
 }
